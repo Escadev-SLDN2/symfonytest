7b4cb85321de4f43c3cca78e498ef5c2ea8de5a2
Bastien BALOUP <35816948+Bastien-Baloup@users.noreply.github.com>
2020-06-25 10:07:07 +0200

    ajout page article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,9 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Categorie;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
+
+    /**
+     * @Route("/blog/article/{id}", name="article")
+     */
+    public function articles($id){
+        $article = $this->getDoctrine()->getRepository( Article::class )->find($id);
+        return $this->render('blog/article.html.twig', [
+            'pagetitle' => $article->getTitre(),
+            'article'  =>  $article,
+        ]);
+    }
+
     /**
      * @Route("/blog/{id}", name="blog")
      */
@@ -15,8 +28,10 @@ class BlogController extends AbstractController
     {
         $categorie = $this->getDoctrine()->getRepository( Categorie::class )->find( $id );
         return $this->render('blog/index.html.twig', [
-            'pagetitle' => $categorie->getNom(),
-            'categorie' => $categorie,
+            'pagetitle' =>  $categorie->getNom(),
+            'categorie' =>  $categorie,
         ]);
     }
+
+    
 }

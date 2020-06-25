ba923b637deaeae4eba651c79f6196ed789ac619
Avignon Camille <camille.avignon15@gmail.com>
2020-06-25 11:26:21 +0200

    article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Categorie;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
@@ -17,6 +18,19 @@ class BlogController extends AbstractController
         return $this->render('blog/index.html.twig', [
             'pagetitle' => $categorie->getNom(),
             'categorie' => $categorie,
+        ]);
+    }
+
+   
+    /**
+     * @Route("/blog/article/{id}", name="article")
+     */
+    public function article($id)
+    {
+        $article = $this->getDoctrine()->getRepository( Article::class )->find( $id );
+        return $this->render('blog/article.html.twig', [
+            'pagetitle' => $article->getTitre(),
+            'article'  =>  $article,
         ]);
     }
 }

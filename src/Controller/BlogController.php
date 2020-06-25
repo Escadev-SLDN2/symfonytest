1091fd0749378244e712e1c88bed85e05cda3763
Nicolas Vauché <hello@nicolasvauche.net>
2020-06-25 14:35:11 +0200

    Article page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,21 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Categorie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Categorie;
 
-class BlogController extends AbstractController
+class BlogController
+    extends AbstractController
 {
+
     /**
      * @Route("/blog/{id}", name="blog")
      */
-    public function index($id)
+    public function index( $id )
     {
         $categorie = $this->getDoctrine()->getRepository( Categorie::class )->find( $id );
-        return $this->render('blog/index.html.twig', [
-            'pagetitle' => $categorie->getNom(),
-            'categorie' => $categorie,
-        ]);
+
+        return $this->render( 'blog/index.html.twig',
+            [
+                'pagetitle' => $categorie->getNom(),
+                'categorie' => $categorie,
+            ] );
+    }
+
+    /**
+     * @Route("/blog/article/{id}", name="blog.article")
+     */
+    public function article( $id )
+    {
+        $article = $this->getDoctrine()->getRepository( Article::class )->find( $id );
+
+        return $this->render( 'blog/article.html.twig',
+            [
+                'pagetitle' => $article->getTitre(),
+                'article'   => $article,
+            ] );
     }
 }
